dcbcfa7e359d45ebc5ff71179adaff6536c2581c
QaraMirza <93111094+QaraMirza@users.noreply.github.com>
2025-01-21 15:44:20 +0300

    Just choose file type / Просто выбери тип файлов

--- Main.py
+++ Main.py
@@ -2,9 +2,9 @@ from learning_algorithms.linear import load_linear
 from learning_algorithms.polynomial import load_polynomial
 from learning_algorithms.neural import load_neural
 
-file_path = "resources/students/normalized.xlsx"
 file_type = "students"  # Перевести в Enum
+file_path = "resources/" + file_type + "/normalized.xlsx"
 
-# load_linear(file_path, file_type)
-# load_polynomial(file_path, file_type)
+load_linear(file_path, file_type)
+load_polynomial(file_path, file_type)
 load_neural(file_path, file_type)
